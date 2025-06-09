a46eef48071985c6d0d40a0d9e728bb224518b94
kruthi03 <kruthikap03@gmail.com>
2025-06-09 14:24:03 +0200

    intial

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 """
 Configuration file for API keys and settings.
 """
-NEWS_API_KEY = "YOUR_NEWS_API_KEY84ec6afae0f44119a277192ecfaa5b5e"
-OPENAI_API_KEY='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'
+NEWS_API_KEY = ""
+OPENAI_API_KEY=''
